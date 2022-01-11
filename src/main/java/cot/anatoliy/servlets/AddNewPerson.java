fadf2ade80d6405987ff5d62d20567557f3c22eb
AnatoliyZhartun <as.zhartun@yandex.by>
2022-01-11 12:56:15 +0300

    Adding servlets and html pages

---
+++ src/main/java/cot/anatoliy/servlets/AddNewPerson.java
@@ -0,0 +1,31 @@
+package cot.anatoliy.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddNewPerson extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("http://localhost:8080/servlets-app/pages/create.html");
+//        req.getRequestDispatcher("pages/create.html").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Create new Entity!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        req.getRequestDispatcher("/pages/index.html").forward(req, resp);
+    }
+}
