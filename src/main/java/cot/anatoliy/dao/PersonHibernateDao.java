bf657e73623860eb712d929de5ef21405f1f054e
AnatoliyZhartun <as.zhartun@yandex.by>
2022-01-27 21:43:23 +0300

    IT IS ALIVE DON'T KNOW WHY

--- src/main/java/cot/anatoliy/dao/PersonHibernateDao.java
+++ src/main/java/cot/anatoliy/dao/PersonHibernateDao.java
@@ -6,19 +6,25 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonHibernateDao implements PersonDao {
-
+    private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
     public static void main(String[] args) {
         final PersonHibernateDao personHibernateDao = new PersonHibernateDao();
-        personHibernateDao.createPerson(new Person("Zhora", 77));
+//        personHibernateDao.createPerson(new Person("Zhora", 77));
+        System.out.println(personHibernateDao.readPersonById(12));
+        final Person person = personHibernateDao.readPersonById(13);
+        person.setAge(44);
+        person.setName("Sarah Connor");
+        personHibernateDao.updatePerson(person);
+        personHibernateDao.deletePerson(13);
     }
 
     @Override
     public void createPerson(Person person) {
-        final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
@@ -36,21 +42,55 @@ public class PersonHibernateDao implements PersonDao {
 
     @Override
     public List<Person> readAllPersons() {
-        return null;
+        List<Person> personList = new ArrayList<>();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            personList.addAll(session.createQuery("from person").list());
+        }
+        return personList;
     }
 
     @Override
     public Person readPersonById(int id) {
-        return null;
+        Transaction transaction = null;
+        Person person = new Person();
+        try (Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            person = session.get(Person.class, id);
+        }
+        return person;
     }
 
     @Override
     public void updatePerson(Person updatedPerson) {
-
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            // do something
+            session.update(updatedPerson);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deletePerson(int id) {
-
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            // do something
+            session.delete(readPersonById(id));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
